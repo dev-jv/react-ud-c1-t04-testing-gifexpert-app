968950612bdf1a90168c402483d74e3ad9ca2fad
dev-jv <dev.javierleon@gmail.com>
2021-01-07 18:08:57 -0500

    three

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const GifExpertApp = () => {
+    // const categories = ['Grimm', 'Fringe', 'The X-Files', 'Supernatural'];
+    const [categories, setCategories] = useState(['Grimm', 'Fringe', 'The X-Files', 'Supernatural']);
+
+    const handleAdd = () => {
+        setCategories([...categories, 'Dr. House']);
+    };
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+
+            <button onClick={ handleAdd }>Agregar</button>
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={ category }>{ category }</li>
+                    })
+                }
+            </ol>
         </>
     )
 };
