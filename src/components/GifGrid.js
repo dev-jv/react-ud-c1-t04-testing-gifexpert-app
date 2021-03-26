1fc39b110227c7daea8e1be4e590809d180f2e24
Javier Leon <dev.javierleon@gmail.com>
2021-03-26 15:05:23 -0500

    thirteen

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -48,8 +48,8 @@ export const GifGrid = ({ category }) => {
 
     return (
         <>
-            <h3>{ category }</h3>
-            { loading ? 'Loading... ' : 'Loaded data'}
+            <h3 className={'animate__animated animate__fadeIn'}>{ category }</h3>
+            { loading && <p className={'animate__animated animate__flash'}>Loading</p>}
             <div className="card-grid">
                 {
                     images.map(img =>
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -10,13 +10,13 @@ export const useFetchGifs = (category) => {
     useEffect( () => {
             getGifs( category )
                 .then( imgs => {
-                    setTimeout( () => {
+                    // setTimeout( () => {
                         console.log(imgs);
                         setState({
                             data: imgs,
                             loading: false
                         })
-                    }, 1000);
+                    // }, 1000);
                 })
     }, [category] );
 
