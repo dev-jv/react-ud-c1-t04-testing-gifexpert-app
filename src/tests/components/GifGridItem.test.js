e3417edf7a1e4a2b125657f86ff0c5c732aaf216
Javier Leon <dev.javierleon@gmail.com>
2021-04-08 17:12:31 -0500

    twenty-one

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -8,11 +8,32 @@ describe('/> Testing in <GifGridItem/>'.bgBlue.white, () => {
 
     const title = 'Any title';
     const url = 'https://localhost/some.jpg';
+    let wrapper = shallow(<GifGridItem title={ title } url={ url }/>);
 
    test('Test 1: Should show <GifGridItem /> correctly ',() => {
-
-       let wrapper = shallow(<GifGridItem title={ title } url={ url }/>);
        expect(wrapper).toMatchSnapshot();
    });
+
+   test('Should have a paragraph with the title', () => {
+       const p = wrapper.find('p');
+       console.log(p.text());
+       console.log(p.text().trim());
+       expect(p.text().trim()).toBe(title);
+   });
+
+    test('Should have an image like in the url, alt in props', () => {
+        const img = wrapper.find('img');
+        // console.log(img.html());
+        console.log(img.props().src);
+        // expect(img.props().src).toBe(url);
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('Should have an animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className.includes('animate__fadeIn')).toBe(true);
+    });
 
 });
